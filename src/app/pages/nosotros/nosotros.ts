a3c421a2f61332dc4577a95fdca6354cd51e97c5
Duvier David <duvierdiegodavid.0715@gmail.com>
2025-09-10 19:59:30 -0500

    Add testimonials section and update styles for improved presentation

--- src/app/pages/nosotros/nosotros.ts
+++ src/app/pages/nosotros/nosotros.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Nav } from '../../shared/nav/nav';
+import { Testimonios } from '../../components/testimonios/testimonios';
 
 @Component({
   selector: 'app-nosotros',
-  imports: [Nav],
+  imports: [Nav, Testimonios],
   templateUrl: './nosotros.html',
   styleUrl: './nosotros.css'
 })
