7dee7fe30fe8c33f0a6b7387eb4e44c4192d7c10
Duvier David <duvierdiegodavid.0715@gmail.com>
2025-09-17 21:10:51 -0500

    Remove imports innecesary

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -3,15 +3,12 @@ import { Header } from '../../shared/header/header';
 import { Nav } from '../../shared/nav/nav';
 import { Footer } from '../../shared/footer/footer';
 
-import { Banner } from '../../components/banner/banner';
-import { Promociones } from '../../components/promociones/promociones';
-import { Newsletter } from '../../components/newsletter/newsletter';
 import { PerfumesH } from '../perfumes-h/perfumes-h';
 import { PerfumesM } from '../perfumes-m/perfumes-m';
 
 @Component({
   selector: 'app-home',
-  imports: [Header, Nav, Footer, Banner, Promociones, Newsletter, PerfumesH, PerfumesM],
+  imports: [Header, Nav, Footer, PerfumesH, PerfumesM],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
